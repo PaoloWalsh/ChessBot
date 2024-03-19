eba7f57bc2d777edfcbb3fb7d818f7b76a5ca9c7
Polwall <paolowalsh7@gmail.com>
2024-03-19 16:23:55 +0100

    created board

--- script.js
+++ script.js
@@ -4,10 +4,54 @@ const rows = 8;
 const cols = 8;
 
 function build () {
+    console.log("hello world!")
     buildBoard();
 }
 
-function buildBoard() {
-    const board = document.getElementById("board");
 
+function buildBoard() {
+    console.log("hello world!")
+    const board = document.getElementById("board");
+    let count = 63;
+    for(let i = rows-1; i >= 0; i--) {
+        for(let j = cols-1; j >= 0; j--){
+            const square = document.createElement("div");
+            square.setAttribute("id", count--);
+            //square.setAttribute("readonly", "readonly");
+            square.classList.add("square");
+            if(!((i+j)%2))
+                square.classList.add("light");
+            else
+                square.classList.add("dark");
+            board.appendChild(square);
+        }
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("hello world!")
+    build();
+});
+
+/*
+function buildBoard() {
+    console.log("hello world!")
+    const board = document.getElementById("board");
+    for(let i = rows-1; i >= 0; i--) {
+        const row = document.createElement("div");
+        row.setAttribute("id", "row: " + i);
+        row.classList.add("rows");
+        board.appendChild(row);
+        for(let j = cols-1; j >= 0; j--){
+            const square = document.createElement("input");
+            square.setAttribute("id", "row, col: "+ i + " " + j);
+            if(!(i+j)%2)
+                square.classList.add("light-square");
+            else
+                square.classList.add("dark-square");
+            row.appendChild(square);
+        }
+    }
+}
+
+*/
