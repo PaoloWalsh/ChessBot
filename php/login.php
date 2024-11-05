a690ccdf5a729bb04e917959be3c379cc138848d
PaoloWalsh <paolowalsh7@gmail.com>
2024-11-05 13:27:31 +0100

    formattato login.php

--- php/login.php
+++ php/login.php
@@ -1,58 +1,47 @@
-
 <?php
 
-$erroreNonEsiste = false;
-$passwordErr = false;
+  $erroreNonEsiste = false;
+  $passwordErr = false;
 
-if(isset($_POST['login'])&&isset($_POST['username'])&&isset($_POST['password'])){
-    
+  if(isset($_POST['login'])&&isset($_POST['username'])&&isset($_POST['password'])){    
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-
     require "dbaccess.php";
     $connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
-             
 
     if(mysqli_connect_errno())
-        die(mysqli_connect_error());
- 
+      die(mysqli_connect_error());
+
     // Prepared Statement
     $query = "select * from utente where Username=?;";
     if($statement = mysqli_prepare($connection, $query)){
-        mysqli_stmt_bind_param($statement, 's', $username);
-
-        mysqli_stmt_execute($statement);
-        $result = mysqli_stmt_get_result($statement);
-        if(mysqli_num_rows($result)==0){
-           $erroreNonEsiste = true;
+      mysqli_stmt_bind_param($statement, 's', $username);
+      mysqli_stmt_execute($statement);
+      $result = mysqli_stmt_get_result($statement);
+      if(mysqli_num_rows($result)==0){
+        $erroreNonEsiste = true;
+      }
+      else {
+        $row = mysqli_fetch_assoc($result); 
+        $hashedPassword = $row["Password"];
+        if (password_verify($password, $hashedPassword)) {
+          // se la pass è corretta, redirect alla home page
+          session_start();
+          $_SESSION["logged"] = true;
+          $_SESSION["username"] = $username;
+          header("Location: index.php");
+        } else {
+          // se la pass è sbagliata, error message
+          $passwordErr = true;
         }
-           
-        
-        else {
-            $row = mysqli_fetch_assoc($result); 
-            $hashedPassword = $row["Password"];
-
-           
-            if (password_verify($password, $hashedPassword)) {
-                // se la pass è corretta, redirect alla home page
-                session_start();
-                $_SESSION["logged"] = true;
-                $_SESSION["username"] = $username;
-                header("Location: index.php");
-           
-            } else {
-                // se la pass è sbagliata, error message
-                $passwordErr = true;
-            }
-        }
-      
+      }
     }
     else{
-        // Close the database connection
-            die(mysqli_connect_error());
-        }
-}
+      // Close the database connection
+      die(mysqli_connect_error());
+    }
+  }
 ?>
 
 <!DOCTYPE html>
