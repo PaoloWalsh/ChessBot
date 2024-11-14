8f34478e257a369215d11bde64d344955f00fc80
PaoloWalsh <paolowalsh7@gmail.com>
2024-11-14 18:16:48 +0100

    pulizia

--- js/game/game-main.js
+++ js/game/game-main.js
@@ -5,7 +5,7 @@ let hostNumberMoves;
 /**
  * @brief viene chiamata dopo che il DOM è stato caricato e avvia la funzione per far partire il gioco
  */
-function build () {
+function build() {
     hostColor = localStorage.getItem('colore');
     hostNumberMoves = 0;
     init_Pieces();
@@ -22,14 +22,14 @@ function build () {
 function buildBoard() {
     const scacchiera = document.getElementById("board");
     let count = 63;
-    for(let i = rows-1; i >= 0; i--) {
-        for(let j = cols-1; j >= 0; j--){
+    for (let i = rows - 1; i >= 0; i--) {
+        for (let j = cols - 1; j >= 0; j--) {
             const square = document.createElement("div");
-            if(board[count])
+            if (board[count])
                 square.appendChild(board[count].getDiv());
             square.setAttribute("id", count--);
             square.classList.add("square");
-            if(!((i+j)%2))
+            if (!((i + j) % 2))
                 square.classList.add("light");
             else
                 square.classList.add("dark");
@@ -43,15 +43,12 @@ function buildBoard() {
  * @brief reimposta il valore di tutte le variabili globali per prepararsi a una nuova partita
  * reset the value of all global variable to prepare for a new game
  */
-function resetGlobalVariables() {   // da aggiustare
+function resetGlobalVariables() {
     white_turn = true;
     black_turn = false;
-
     white_in_check = false;
     black_in_check = false;
-
     enPassantPlayed = false;
-
     firstClick = true;
 }
 
@@ -75,37 +72,37 @@ function init_drag() {
 
 //global variables
 let firstClick = true;
-let firstPieceClicked; 
+let firstPieceClicked;
 let destinationSquare;
 let startPositionId;
 let draggedElement;
 let draggedPiece;
 
-function click (e) {    //da aggiustare
+function click(e) {    //da aggiustare
     removeSelectedSquares();
     let currentElement = e.target;
     let pieceElement;
     let pieceClicked = false;
     let squareCliecked = false;
-    if(isPieceElement(currentElement)){
+    if (isPieceElement(currentElement)) {
         pieceElement = getPiece(currentElement.id);
         pieceClicked = true;
-    } else 
+    } else
         squareCliecked = true;
-    
-    if(firstClick){
-        if(squareCliecked) return;
-        if((pieceElement.color == "white" && black_turn) || (pieceElement.color == "black" && white_turn)) return;
+
+    if (firstClick) {
+        if (squareCliecked) return;
+        if ((pieceElement.color == "white" && black_turn) || (pieceElement.color == "black" && white_turn)) return;
         firstPieceClicked = pieceElement;
         selectLandingSquares(firstPieceClicked);
         firstClick = false;
         return;
     }
 
-    if(pieceClicked){
-        if(firstPieceClicked.color == pieceElement.color){
-            if(!((firstPieceClicked.id.includes("king") && pieceElement.id.includes("rook")) || (firstPieceClicked.id.includes("rook") && pieceElement.id.includes("king")))){
-                if((pieceElement.color == "white" && black_turn) || (pieceElement.color == "black" && white_turn)) return;
+    if (pieceClicked) {
+        if (firstPieceClicked.color == pieceElement.color) {
+            if (!((firstPieceClicked.id.includes("king") && pieceElement.id.includes("rook")) || (firstPieceClicked.id.includes("rook") && pieceElement.id.includes("king")))) {
+                if ((pieceElement.color == "white" && black_turn) || (pieceElement.color == "black" && white_turn)) return;
                 firstPieceClicked = pieceElement;
                 selectLandingSquares(firstPieceClicked);
                 firstClick = false;
@@ -114,19 +111,19 @@ function click (e) {    //da aggiustare
         }
     }
 
-    if(makeMove(firstPieceClicked, currentElement))
+    if (makeMove(firstPieceClicked, currentElement))
         firstClick = true;
-    
+
 }
 
-function dragStart (e) {
+function dragStart(e) {
     startPositionId = e.target.parentNode.getAttribute('id');   //square id
     draggedElement = e.target;
     let piece = getPiece(draggedElement.id);
-    selectLandingSquares(piece);     
+    selectLandingSquares(piece);
 }
 
-function dragOver (e) {
+function dragOver(e) {
     e.preventDefault();
 }
 
@@ -136,7 +133,7 @@ function dragOver (e) {
  * @param {*} e target dell'evento 
  * @returns 
  */
-function dragDrop (e) {
+function dragDrop(e) {
     let piece; // is the piece that is being moved
     piece = getPiece(draggedElement.id);
     let square = e.target;
@@ -150,53 +147,50 @@ function dragDrop (e) {
  * @param {*} square l'elemento di destinazione HTML può essere una casella o un pezzo
  * @returns restituisce true se la mossa è stata effettuata, false altrimenti.
  */
- async function makeMove(piece, square) {  //game logic    //da aggiustare
-    removeSelectedSquares();     
+async function makeMove(piece, square) {  //game logic    //da aggiustare
+    removeSelectedSquares();
     let moveMade = false;
     let castlignRook;
     let element = pieceToDiv(piece);
-    if((white_turn && element.id.includes("white"))
-        || (black_turn && element.id.includes("black"))){
+    if ((white_turn && element.id.includes("white"))
+        || (black_turn && element.id.includes("black"))) {
         // if true -> the target square is empty
-        if(!square.hasChildNodes())
-        {
-            if(!moveWithCheck(square, piece)) return;
+        if (!square.hasChildNodes()) {
+            if (!moveWithCheck(square, piece)) return;
             moveMade = validate_move(square, piece, true);
-            if(moveMade){
+            if (moveMade) {
                 square.append(element);
-                if(enPassantPlayed){
+                if (enPassantPlayed) {
                     capture_sound.play();
                     enPassantPlayed = false;
                 }
                 else
                     move_sound.play();
             }
-            
+
         }
         //check if the target square has a same colored piece
         else if ((piece.id.includes("white") && square.id.includes("black"))
-            || (piece.id.includes("black") && square.id.includes("white")))
-        {
+            || (piece.id.includes("black") && square.id.includes("white"))) {
             //if true -> the target square has a different color piece
             destinationSquare = square.parentNode;
-            if(!moveWithCheck(destinationSquare, piece)) return;    
+            if (!moveWithCheck(destinationSquare, piece)) return;
             moveMade = validate_move(destinationSquare, piece, true);
-            if(moveMade) {
+            if (moveMade) {
                 square.parentNode.append(element);
                 square.remove();
                 capture_sound.play();
-                
+
             }
         }
         else if ((element.id.includes("white_king") && square.id.includes("white_rook"))
             || (element.id.includes("black_king") && square.id.includes("black_rook"))
-        )
-        {   
+        ) {
             castlignRook = getPiece(square.id);
             destinationSquare = square.parentNode;
-            if(!moveWithCheck(destinationSquare, piece)) return;
+            if (!moveWithCheck(destinationSquare, piece)) return;
             moveMade = validate_move(destinationSquare, piece, true);
-            if(moveMade) {
+            if (moveMade) {
                 let rookStartSquare = square.parentNode;
                 let kingStartSquare = element.parentNode;
                 let rookEndSquare;
@@ -205,13 +199,13 @@ function dragDrop (e) {
                 let rookIndex = parseInt(rookStartSquare.getAttribute("id"));
                 element.remove();
                 square.remove();
-                if(kingIndex > rookIndex){  //I'm castling to the right
-                    rookEndSquare = document.getElementById((kingIndex-1)+'');
-                    kingEndSquare = document.getElementById((rookIndex+1)+'');
+                if (kingIndex > rookIndex) {  //I'm castling to the right
+                    rookEndSquare = document.getElementById((kingIndex - 1) + '');
+                    kingEndSquare = document.getElementById((rookIndex + 1) + '');
                 }
-                else{
-                    rookEndSquare = document.getElementById((kingIndex+1)+'');
-                    kingEndSquare = document.getElementById((rookIndex-2)+'');
+                else {
+                    rookEndSquare = document.getElementById((kingIndex + 1) + '');
+                    kingEndSquare = document.getElementById((rookIndex - 2) + '');
                 }
                 rookEndSquare.append(square);
                 kingEndSquare.append(element);
@@ -219,22 +213,22 @@ function dragDrop (e) {
             }
         }
     }
-    if(moveMade){
-        if(promotingMove){
+    if (moveMade) {
+        if (promotingMove) {
             await handleDialog(piece);
         }
         promotingMove = false;
-        if(((hostColor === 'bianco') && white_turn ) || ((hostColor === 'nero') && black_turn ))
+        if (((hostColor === 'bianco') && white_turn) || ((hostColor === 'nero') && black_turn))
             hostNumberMoves++;
         updateBoard(castlignRook);
-        checkCheck();    
+        checkCheck();
         switchTurn();
         updateMessages();       //qui controlliamo lo scacco matto
-        boardIsConsistent();    //used for debug
+        boardIsConsistent();    //debug info 
         return true;
     }
     return false;
-    
+
 }
 
 
@@ -249,14 +243,14 @@ async function handleDialog(oldPiece) {
     const tail = ".png";
     let promotionOptions = ['queen', 'rook', 'bishop', 'knight'];
     let imgs = [];
-    for(let i = 0; i < 4; i++){
+    for (let i = 0; i < 4; i++) {
         const container = document.createElement('div');
         container.classList.add('sub-container');
         overContainer.appendChild(container);
         const img = document.createElement('img');
-        img.src = head+promotionOptions[i]+tail;
+        img.src = head + promotionOptions[i] + tail;
         img.alt = promotionOptions[i];
-        img.id = head.split('/')[3]+promotionOptions[i];
+        img.id = head.split('/')[3] + promotionOptions[i];
         imgs.push(img);
         container.appendChild(img);
     }
@@ -265,7 +259,7 @@ async function handleDialog(oldPiece) {
     createPromotionPiece(idImg, oldPiece);
 }
 
-function setImgEvent (imgs) {       //get data
+function setImgEvent(imgs) {       //get data
     return new Promise((resolve) => {
         imgs.forEach(img => {
             img.addEventListener("click", (event) => {
@@ -274,7 +268,7 @@ function setImgEvent (imgs) {       //get data
                 const overContainer = document.getElementById('over-container-flex');
                 removeAllChildren(overContainer);
                 resolve(event.target.id); // Risolve con il bottone che è stato cliccato
-            }); 
+            });
         });
     });
 }
@@ -283,12 +277,12 @@ function setImgEvent (imgs) {       //get data
  * @brief handles the logic of the promotion, 
  * is called when the user clicks on the img of the piece it wants to promote to
  */
-function promotionClick (event) {
+function promotionClick(event) {
     const img = event.target;
     return img.id;
 }
 
-function createPromotionPiece(idImg, oldPiece){
+function createPromotionPiece(idImg, oldPiece) {
     let newPieceColor = idImg.split('_')[0];
     let newPieceType = idImg.split('_')[1];
     let oldPieceColor = newPieceColor;
@@ -300,12 +294,12 @@ function createPromotionPiece(idImg, oldPiece){
 
     let newPiece;
     let numberPiece;
-    let colorHead = oldPieceColor+'_';
+    let colorHead = oldPieceColor + '_';
     switch (newPieceType) {
         case 'knight':
             numberPiece = (newPieceColor == 'white') ? numberWhiteKnights : numberBlackKnights;
-            newPiece = new piece(colorHead+'knight', numberPiece);
-            if(newPieceColor == 'white'){
+            newPiece = new piece(colorHead + 'knight', numberPiece);
+            if (newPieceColor == 'white') {
                 white_knights.push(newPiece);
                 numberWhiteKnights++;
             } else {
@@ -315,8 +309,8 @@ function createPromotionPiece(idImg, oldPiece){
             break;
         case 'bishop':
             numberPiece = (newPieceColor == 'white') ? numberWhiteBishops : numberBlackBishops;
-            newPiece = new piece(colorHead+'bishop', numberPiece);
-            if(newPieceColor == 'white'){
+            newPiece = new piece(colorHead + 'bishop', numberPiece);
+            if (newPieceColor == 'white') {
                 white_bishops.push(newPiece);
                 numberWhiteBishops++;
             } else {
@@ -326,8 +320,8 @@ function createPromotionPiece(idImg, oldPiece){
             break;
         case 'rook':
             numberPiece = (newPieceColor == 'white') ? numberWhiteRooks : numberBlackRooks;
-            newPiece = new piece(colorHead+'rook', numberPiece);
-            if(newPieceColor == 'white'){
+            newPiece = new piece(colorHead + 'rook', numberPiece);
+            if (newPieceColor == 'white') {
                 white_rooks.push(newPiece);
                 numberWhiteRooks++;
             } else {
@@ -335,10 +329,10 @@ function createPromotionPiece(idImg, oldPiece){
                 numberBlackRooks++;
             }
             break;
-            case 'queen':
+        case 'queen':
             numberPiece = (newPieceColor == 'white') ? numberWhiteQueens : numberBlackQueens;
-            newPiece = new piece(colorHead+'queen', numberPiece);
-            if(newPieceColor == 'white'){
+            newPiece = new piece(colorHead + 'queen', numberPiece);
+            if (newPieceColor == 'white') {
                 white_queens.push(newPiece);
                 numberWhiteQueens++;
             } else {
@@ -359,12 +353,12 @@ function createPromotionPiece(idImg, oldPiece){
     oldPieceDiv.id = newPiece.id;
     const head = "../img/png/";
     const tail = ".png";
-    childImg.src = head+(newPiece.id).slice(0, -1)+tail;
+    childImg.src = head + (newPiece.id).slice(0, -1) + tail;
 
-    if(oldPieceColor == 'white'){
+    if (oldPieceColor == 'white') {
         white_pieces.push(newPiece);
     }
-    else{
+    else {
         black_pieces.push(newPiece);
     }
     draggedPiece = newPiece;
@@ -373,7 +367,7 @@ function createPromotionPiece(idImg, oldPiece){
     console.log(draggedPiece);
 }
 
-    
+
 
 const tuoTurno = "È il tuo turno!";
 const punteggioBene = "Sei in vantaggio di: ";
@@ -386,7 +380,7 @@ const scaccoMattoMale = "Scacco matto, hai perso! :(";
 /**
  * @brief aggiorna i messaggi per il giocatore, come chi è di turno, se qualcuno è sotto scacco o se la partita è terminata, controlla anche lo scacco matto chiamando checkMate
  */
-function updateMessages () {
+function updateMessages() {
     let bpTurno = document.getElementById('bp-turno');
     let bpPunteggio = document.getElementById('bp-punteggio');
     let bpScacco = document.getElementById('bp-scacco');
@@ -396,19 +390,19 @@ function updateMessages () {
     let wpScacco = document.getElementById('wp-scacco');
     let wpVittoria = document.getElementById('wp-vittoria');
 
-    if(white_turn){
+    if (white_turn) {
         wpTurno.innerText = tuoTurno;
         bpTurno.innerText = "";
     }
-    else{
+    else {
         bpTurno.innerText = tuoTurno;
         wpTurno.innerText = "";
     }
-    if(white_in_check){
+    if (white_in_check) {
         wpScacco.innerText = scaccoMale;
         bpScacco.innerText = scaccoBene;
     }
-    else if (black_in_check){
+    else if (black_in_check) {
         bpScacco.innerText = scaccoMale;
         wpScacco.innerText = scaccoBene;
     }
@@ -417,26 +411,26 @@ function updateMessages () {
         wpScacco.innerText = "";
     }
     let score_value = 0;
-    for(let i = 0; i < 16; i++){
-        if(white_pieces[i].captured)
+    for (let i = 0; i < 16; i++) {
+        if (white_pieces[i].captured)
             score_value += white_pieces[i].value;
-        if(black_pieces[i].captured)
+        if (black_pieces[i].captured)
             score_value += black_pieces[i].value;
     }
-    if(score_value < 0){
+    if (score_value < 0) {
         wpPunteggio.innerText = punteggioBene + (-score_value) + " punti!";
         bpPunteggio.innerText = punteggioMale + (-score_value) + " punti!";
     }
-    else if(score_value > 0){
+    else if (score_value > 0) {
         bpPunteggio.innerText = punteggioBene + (score_value) + " punti!";
         wpPunteggio.innerText = punteggioMale + (score_value) + " punti!";
     }
-    else{
+    else {
         bpPunteggio.innerText = "";
         wpPunteggio.innerText = "";
     }
     let t = checkMate();
-    if(t){
+    if (t) {
         const checkDialog = document.getElementById('checkmate-dialog');
         const whitePlayerName = document.getElementById('wp-nome').innerText;
         const blackPlayerName = document.getElementById('bp-nome').innerText;
@@ -446,22 +440,22 @@ function updateMessages () {
         checkDialog.show();
         let vittoria;
         let colore = localStorage.getItem('colore');
-        if(colore == "nero"){
-            if(white_in_check)
+        if (colore == "nero") {
+            if (white_in_check)
                 vittoria = true;
             else
                 vittoria = false;
         }
-        else{
-            if(black_in_check)
+        else {
+            if (black_in_check)
                 vittoria = true;
             else
                 vittoria = false;
-        } 
+        }
 
         insertMatchDB(vittoria);
     }
-    else{
+    else {
         wpVittoria.innerText = "";
         bpVittoria.innerText = "";
     }
